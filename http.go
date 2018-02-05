0a9a54f8fac98448eeb59279e25fd8668fc3394c
Kare Koho <kare.koho@eficode.com>
2018-02-05 10:58:08 +0200

    Remove double newline

--- http.go
+++ http.go
@@ -68,7 +68,7 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Fprintf(w, "Node name: %s Container ID: %s Container name: %s\n",
+		fmt.Fprintf(w, "Node name: %s Container ID: %s Container name: %s",
 			s.TrimRight(string(hostnameNode), "\n"), ci.ID, ci.name)
 	})
 
